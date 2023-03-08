b1abad38719fa07a46a864eda57226ee96d546c7
programmerkoji <60305092+programmerkoji@users.noreply.github.com>
2023-03-08 22:38:30 +0900

    ポートフォリオに英単語管理アプリを追加

--- page-portfolio.php
+++ page-portfolio.php
@@ -127,6 +127,70 @@
 </section>
 <!-- /.bl_portfolio -->
 
+<section class="bl_portfolio">
+  <div class="ly_cont_inner">
+
+    <h2 class="el_mainTtl">英単語管理アプリ</h2>
+
+    <div class="bl_media">
+      <div class="bl_media_imgWrapper">
+        <a href="https://from-forties.net/english-words/login" target="_blank" class="bl_media_imgLink">
+          <figure class="bl_media_img">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/portfolio/english_word_img.png" width="16" height="9" alt="">
+            <figcaption>ログイン後の画面</figcaption>
+          </figure>
+        </a>
+      </div>
+      <!-- /.bl_media_imgWrapper -->
+      <div class="bl_media_body bl_detail">
+        <h3 class="bl_detail_ttl">サイトURL</h3>
+        <p>ユーザー：<a href="https://from-forties.net/english-words/login" target="_blank">https://from-forties.net/english-words/login</a></p>
+        <h3 class="bl_detail_ttl">アプリ概要</h3>
+        <p>英単語の記憶度を管理するアプリ</p>
+        <h3 class="bl_detail_ttl">なぜ作成したのか</h3>
+        <p>
+          家族が英語の勉強をしているときに、英単語の記憶度を管理できるアプリがあると便利だというつぶやきからヒント得て作成しました。<br>
+          
+        </p>
+        <h3 class="bl_detail_ttl">機能一覧</h3>
+        <div class="bl_functionList">
+          <div class="bl_functionList_tableWrapper">
+            <table>
+              <tbody>
+                <tr>
+                  <td>ログイン機能、絞り込み検索、並び替え機能、ページネーション、モーダル機能など</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <h3 class="bl_detail_ttl">工夫したところ</h3>
+        <ul class="el_bulletList">
+          <li>別ユーザーが登録した単語の編集画面等に対し、URL直打ちでアクセスしたときに404を表示させる</li>
+          <li>削除の際に、ポップアップで削除前の確認を表示させる</li>
+        </ul>
+        <h3 class="bl_detail_ttl">使用技術</h3>
+        <ul class="el_bulletList">
+          <li>Laravel 8</li>
+          <li>Laravel Breeze</li>
+          <li>Micromodal.js</li>
+        </ul>
+        <h3 class="bl_detail_ttl">その他</h3>
+        <p>開発環境はMAMP、本番環境はさくらサーバーです。</p>
+        <p class="mt-1em">
+          その他の詳細についてはGitHubにてご確認をお願いします。<br>
+          <a href="https://github.com/programmerkoji/english_words" target="_blank">GitHubはこちら</a>
+        </p>
+        
+      </div>
+      <!-- /.bl_media_body -->
+    </div>
+    <!-- /.bl_media -->
+  </div>
+  <!-- /.ly_cont_inner -->
+</section>
+<!-- /.bl_portfolio -->
+
 </main>
 
 <?php get_footer(); ?>
