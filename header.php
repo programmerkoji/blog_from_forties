fd8606ccb4219b12e93df31a5c1fbb6d8452ee0b
Koji Sato <utautaouka@gmail.com>
2025-01-13 09:13:09 +0900

    gtagの追加

--- header.php
+++ header.php
@@ -5,6 +5,16 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-XED0FFQ7L0"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-XED0FFQ7L0');
+</script>
+
 <?php $title ="";
 if(is_home() || is_front_page()) {
 }elseif(is_page()) {
